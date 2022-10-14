5809ed1bb3d4c46a29f7c9e18992e8c360b1d2e7
Fabian Polvin <polvin.fabian@gmail.com>
2022-10-14 16:50:58 +0200

    update TestRemplissageBDD

--- src/test/java/test/TestRemplissageBDD.java
+++ src/test/java/test/TestRemplissageBDD.java
@@ -5,14 +5,24 @@ import java.time.LocalDate;
 import model.Ingredient;
 
 public class TestRemplissageBDD {
-	Ingredient ingredient0 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient1 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient2 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient3 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient4 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient5 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient6 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient7 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient8 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
-	Ingredient ingredient9 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient00 = new Ingredient(00, LocalDate.of(2020, 1, 10), LocalDate.of(2020, 12, 20), false, false);
+	Ingredient ingredient01 = new Ingredient(10, LocalDate.of(2020, 1, 10), LocalDate.of(2020, 7, 20), false, true);
+	Ingredient ingredient02 = new Ingredient(20, LocalDate.of(2020, 2, 10), LocalDate.of(2020, 10, 20), true, true);
+	Ingredient ingredient03 = new Ingredient(30, LocalDate.of(2020, 2, 10), LocalDate.of(2020, 8, 20), true, false);
+	Ingredient ingredient04 = new Ingredient(40, LocalDate.of(2020, 3, 10), LocalDate.of(2020, 11, 20), false, true);
+	Ingredient ingredient05 = new Ingredient(50, LocalDate.of(2020, 3, 10), LocalDate.of(2020, 5, 20), true, false);
+	Ingredient ingredient06 = new Ingredient(60, LocalDate.of(2020, 4, 10), LocalDate.of(2020, 4, 20), true, false);
+	Ingredient ingredient07 = new Ingredient(70, LocalDate.of(2020, 4, 10), LocalDate.of(2020, 9, 20), false, true);
+	Ingredient ingredient08 = new Ingredient(80, LocalDate.of(2020, 5, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient09 = new Ingredient(90, LocalDate.of(2020, 5, 10), LocalDate.of(2020, 12, 20), false, true);
+	Ingredient ingredient10 = new Ingredient(00, LocalDate.of(2020, 1, 10), LocalDate.of(2020, 12, 20), false, false);
+	Ingredient ingredient11 = new Ingredient(10, LocalDate.of(2020, 1, 10), LocalDate.of(2020, 7, 20), true, true);
+	Ingredient ingredient12 = new Ingredient(20, LocalDate.of(2020, 2, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient13 = new Ingredient(30, LocalDate.of(2020, 2, 10), LocalDate.of(2020, 8, 20), false, true);
+	Ingredient ingredient14 = new Ingredient(40, LocalDate.of(2020, 3, 10), LocalDate.of(2020, 11, 20), true, false);
+	Ingredient ingredient15 = new Ingredient(50, LocalDate.of(2020, 3, 10), LocalDate.of(2020, 5, 20), true, false);
+	Ingredient ingredient16 = new Ingredient(60, LocalDate.of(2020, 4, 10), LocalDate.of(2020, 4, 20), false, true);
+	Ingredient ingredient17 = new Ingredient(70, LocalDate.of(2020, 4, 10), LocalDate.of(2020, 9, 20), true, false);
+	Ingredient ingredient18 = new Ingredient(80, LocalDate.of(2020, 5, 10), LocalDate.of(2020, 10, 20), false, true);
+	Ingredient ingredient19 = new Ingredient(90, LocalDate.of(2020, 5, 10), LocalDate.of(2020, 12, 20), false, true);
 }
