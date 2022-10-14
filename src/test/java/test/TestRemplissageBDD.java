f14a8952e86fb09696d121d5b6152e43aa22594f
Fabian Polvin <polvin.fabian@gmail.com>
2022-10-14 16:46:37 +0200

    create: TestRemplissageBDD

---
+++ src/test/java/test/TestRemplissageBDD.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.time.LocalDate;
+
+import model.Ingredient;
+
+public class TestRemplissageBDD {
+	Ingredient ingredient0 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient1 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient2 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient3 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient4 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient5 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient6 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient7 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient8 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+	Ingredient ingredient9 = new Ingredient(10, LocalDate.of(2020, 10, 10), LocalDate.of(2020, 10, 20), false, false);
+}
