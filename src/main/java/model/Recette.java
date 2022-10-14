dd16e2a5fe7dbae4a298acd3b13871e4d05656fc
talwego <talweg42@proton.me>
2022-10-14 17:18:38 +0200

    recette

--- src/main/java/model/Recette.java
+++ src/main/java/model/Recette.java
@@ -37,13 +37,13 @@ public class Recette {
 	}
 	
 
-	public Recette(boolean vegetarien, boolean vegan, List<RecetteIngredient> ingredients, int calorie,
+	public Recette(boolean vegetarien, boolean vegan, List<RecetteIngredient> RecetteIngredients, int calorie,
 			LocalDate debutSaison, LocalDate finSaison, String commentaires, int notes, LocalTime tempsDeCuisine,
 			Boolean isValid) {
 
 		this.vegetarien = vegetarien;
 		this.vegan = vegan;
-		this.ingredients = ingredients;
+		this.RecetteIngredients = RecetteIngredients;
 		this.calorie = calorie;
 		this.debutSaison = debutSaison;
 		this.finSaison = finSaison;
@@ -53,111 +53,113 @@ public class Recette {
 		this.isValid = isValid;
 	}
 
-	
-	public List<RecetteIngredient> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(List<RecetteIngredient> ingredients) {
-		this.ingredients = ingredients;
-	}
 
 	public Integer getId() {
 		return id;
 	}
 
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 
 	public boolean isVegetarien() {
 		return vegetarien;
 	}
 
+
 	public void setVegetarien(boolean vegetarien) {
 		this.vegetarien = vegetarien;
 	}
+
 
 	public boolean isVegan() {
 		return vegan;
 	}
 
+
 	public void setVegan(boolean vegan) {
 		this.vegan = vegan;
 	}
+
+
+	public List<RecetteIngredient> getRecetteIngredients() {
+		return RecetteIngredients;
+	}
+
+
+	public void setRecetteIngredients(List<RecetteIngredient> recetteIngredients) {
+		RecetteIngredients = recetteIngredients;
+	}
+
 
 	public int getCalorie() {
 		return calorie;
 	}
 
+
 	public void setCalorie(int calorie) {
 		this.calorie = calorie;
 	}
+
 
 	public LocalDate getDebutSaison() {
 		return debutSaison;
 	}
 
+
 	public void setDebutSaison(LocalDate debutSaison) {
 		this.debutSaison = debutSaison;
 	}
+
 
 	public LocalDate getFinSaison() {
 		return finSaison;
 	}
 
+
 	public void setFinSaison(LocalDate finSaison) {
 		this.finSaison = finSaison;
 	}
+
 
 	public String getCommentaires() {
 		return commentaires;
 	}
 
+
 	public void setCommentaires(String commentaires) {
 		this.commentaires = commentaires;
 	}
+
 
 	public int getNotes() {
 		return notes;
 	}
 
+
 	public void setNotes(int notes) {
 		this.notes = notes;
 	}
+
 
 	public LocalTime getTempsDeCuisine() {
 		return tempsDeCuisine;
 	}
 
+
 	public void setTempsDeCuisine(LocalTime tempsDeCuisine) {
 		this.tempsDeCuisine = tempsDeCuisine;
 	}
+
 
 	public Boolean getIsValid() {
 		return isValid;
 	}
 
+
 	public void setIsValid(Boolean isValid) {
 		this.isValid = isValid;
 	}
-
-	@Override
-	public String toString() {
-		return "Recette [id=" + id + ", vegetarien=" + vegetarien + ", vegan=" + vegan + ", calorie=" + calorie
-				+ ", debutSaison=" + debutSaison + ", finSaison=" + finSaison + ", commentaires=" + commentaires
-				+ ", notes=" + notes + ", tempsDeCuisine=" + tempsDeCuisine + ", isValid=" + isValid + "]";
-	}
-
-
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
 }
