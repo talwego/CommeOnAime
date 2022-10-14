87b600854e668ace3b01c5256334496cef1f5cc7
Fabian Polvin <polvin.fabian@gmail.com>
2022-10-14 15:58:38 +0200

    add: IDAO, IDAOCompte, DAOCompte(incomplete)

--- src/main/java/dao/IDAO.java
+++ src/main/java/dao/IDAO.java
@@ -4,16 +4,9 @@ import java.util.List;
 
 public interface IDAO<T,K> {
 
-	String urlBdd = "jdbc:mysql://localhost:3306/hopital"; //Windows or Linux
-	//String urlBdd = "jdbc:mysql://localhost:8889/space_project"; //Mac
-	String loginBdd = "root";
-	String passwordBdd=""; //Windows or Linux
-	//String passwordBdd="root" //Mac
-	
 	public List<T> findAll();
 	public T findById(K id);
-	public void insert(T obj);
-	public void update(T obj);
+	public T save(T obj);
 	public void delete(K id);
 	
 }
