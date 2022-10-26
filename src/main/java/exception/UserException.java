b95c04f6c45b94b4f6d592e3ba5e101cb2fa8512
Clement Raymond <clementraymond2@gmail.com>
2022-10-26 17:20:08 +0200

    user

---
+++ src/main/java/exception/UserException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class UserException extends RuntimeException {
+	
+public UserException() {
+		
+	}
+	
+
+	public UserException(String message) {
+		super(message);
+	}
+
+}
