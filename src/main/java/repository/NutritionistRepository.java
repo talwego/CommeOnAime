75ac6f1f69c8e6a6e722882aefc84c1ca552f971
TiiToOnZ <thibautpb@hotmail.fr>
2022-10-26 17:25:50 +0200

    Fix

--- src/main/java/repository/NutritionistRepository.java
+++ src/main/java/repository/NutritionistRepository.java
@@ -3,11 +3,14 @@ package repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import model.Nutritionist;
 
 public interface NutritionistRepository extends JpaRepository<Nutritionist, Integer>{
 
-	Optional<Nutritionist> findByIdFetchMessage(Integer id);
+	@Query("select n from Nutritionist n left join fetch n.message where n.id=:id")
+	Optional<Nutritionist> findByIdFetchMessage(@Param("id")Integer id);
 
 }
