08c438646aef523919f6205050122977a1cf45f1
Clement Raymond <clementraymond2@gmail.com>
2022-11-07 14:12:02 +0100

    jsonViews

---
+++ DiscuissonFront/src/main/java/perso/persoEshopSpringMVC/controller/JsonViews.java
@@ -0,0 +1,20 @@
+package perso.persoEshopSpringMVC.controller;
+
+public class JsonViews {
+
+	public static class Common{
+		
+	}
+	
+	public static class ProduitWithFournisseur extends Common{
+		
+	}
+	
+	public static class FournisseurWithProduit extends Common{
+		
+	}
+	
+	public static class Achat extends Common{
+		
+	}
+}
