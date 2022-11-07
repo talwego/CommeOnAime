bdee5ed3bf162d3d77d175070d378bcaf609077f
Clement Raymond <clementraymond2@gmail.com>
2022-11-07 12:06:48 +0100

    AdminCompteUser

---
+++ DiscuissonFront/src/main/java/perso/persoEshopSpringMVC/restcontroller/UserRestController.java
@@ -0,0 +1,79 @@
+package perso.persoEshopSpringMVC.restcontroller;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+
+import ajc.sopra.eshop.model.JsonViews;
+import model.Admin;
+import model.User;
+import service.AdminService;
+import service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserRestController {
+
+	@Autowired
+	private UserService userSrv;
+
+	@GetMapping("")
+	@JsonView(JsonViews.Common.class)
+	public List<User> findAll() {
+		return userSrv.findAll();
+	}
+
+	@JsonView(JsonViews.Common.class)
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Integer id) {
+		return userSrv.findById(id);
+	}
+
+
+	@PostMapping("")
+	@JsonView(JsonViews.Common.class)
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public User create(@Valid @RequestBody User user, BindingResult br) {
+		if (br.hasErrors()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		
+		return userSrv.save(user);
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(JsonViews.Common.class)
+	public User update(@Valid @RequestBody User user, BindingResult br, @PathVariable Integer id) {
+		if (br.hasErrors() && userSrv.findById(id) == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		return userSrv.save(user);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteId(@PathVariable Integer id) {
+		userSrv.deleteId(id);
+	}
+}
