25784076bc8954fd69966a2f005d53fa2e317fb7
talwego <talweg42@proton.me>
2022-11-18 10:47:12 +0100

    menu

--- DiscuissonAngular/src/app/app.module.ts
+++ DiscuissonAngular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SertARienComponent } from './discuisson-angular/component/sert-arien/sert-arien.component';
 import { SertARienDirective } from './discuisson-angular/directive/sert-arien.directive';
+import { MenuComponent } from './discuisson-angular/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SertARienComponent,
-    SertARienDirective
+    SertARienDirective,
+    MenuComponent
   ],
   imports: [
     BrowserModule
