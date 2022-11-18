e07a519e801fc0f407c143055011144a35d6a112
TiiToOnZ <thibautpb@hotmail.fr>
2022-11-18 14:41:25 +0100

    register

--- DiscuissonAngular/src/app/app.module.ts
+++ DiscuissonAngular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule ,HTTP_INTERCEPTORS} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,16 +10,33 @@ import { routes } from './routes';
 import { IngredientComponent } from './discuisson-angular/component/ingredient/ingredient.component';
 import { SecretComponent } from './secret/secret.component';
 
+import { TokenInterceptor } from './helpers/token.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './Register/register-page/register-page.component';
+
 @NgModule({
-  declarations: [AppComponent, SertARienDirective, IngredientComponent, SecretComponent],
+  declarations: [AppComponent, SertARienDirective, IngredientComponent, SecretComponent, AuthenticationClientComponent, LoginPageComponent, RegisterPageComponent],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- DiscuissonAngular/src/app/routes.ts
+++ DiscuissonAngular/src/app/routes.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IngredientComponent } from './discuisson-angular/component/ingredient/ingredient.component';
+import { AuthGuard } from './helpers/auth.guard';
 import { SecretComponent } from './secret/secret.component';
 
 export const routes: Routes = [
   { path: 'home', component: IngredientComponent },
   { path: 'ingredient', component: IngredientComponent },
 
-{ path: '',  component: SecretComponent, },
+{ path: '',  component: SecretComponent,  canActivate: [AuthGuard],
+},
 
 ];
 
